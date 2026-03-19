406d8fa183f9c687dd6a684034e350c8b0e7937a
Yogesh Chaudhary <yogesh.chaudhary@okta.com>
2026-03-19 10:59:38 +0530

    Replace deprecated aws-sdk v2 with @aws-sdk/client-s3 v3

--- aws/index.js
+++ aws/index.js
@@ -2,11 +2,12 @@
 
 var Rx = require('rx');
 var from = Rx.Observable.from;
-var s3 = require('@auth0/s3');
-var walk = require('walk');
-var path = require('path');
-var client = s3.createClient({});
+var { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+var fs = require('fs');
+var mime = require('mime');
 var files = require('../files');
+
+var client = new S3Client({});
 
 var uploader = function (version, options) {
   return from(options.localPaths).map(function (directoryPath) {
@@ -24,25 +25,34 @@ var uploader = function (version, options) {
     if (options.dry) {
       logger.debug(`Starting upload with following S3 config ${logger.pretty(params)}`);
       return files
-      .walk(params.localDir)
-      .map((localFile) => localFile.replace(directoryPath, version.remotePath));
+        .walk(params.localDir)
+        .map((localFile) => localFile.replace(directoryPath, version.remotePath));
     }
-    return Rx.Observable.create(function (observer) {
-      logger.debug(`Starting upload with following S3 config ${logger.pretty(params)}`);
-      var uploader = client.uploadDir(params);
-      uploader.on('error', function(err) {
-        observer.onError(err);
+    logger.debug(`Starting upload with following S3 config ${logger.pretty(params)}`);
+    return files.walk(directoryPath).flatMap(function (localFile) {
+      var key = localFile.replace(directoryPath, version.remotePath);
+      return Rx.Observable.create(function (observer) {
+        client.send(new PutObjectCommand({
+          Bucket: options.bucket,
+          Key: key,
+          Body: fs.createReadStream(localFile),
+          ContentType: mime.getType(localFile),
+          CacheControl: version.cache,
+          ACL: 'public-read'
+        }))
+          .then(function () {
+            observer.onNext(key);
+            observer.onCompleted();
+          })
+          .catch(function (err) {
+            if (!observer.isStopped) {
+              observer.onError(err);
+            }
+          });
+        return function () {};
       });
-      uploader.on('fileUploadStart', function (path, key) {
-        observer.onNext(key);
-      });
-      uploader.on('end', function() {
-        observer.onCompleted();
-      });
-      return function() {};
     });
   });
-
 };
 
 module.exports = {
